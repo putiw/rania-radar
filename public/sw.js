cbdaaac44d2dc6426d587dc5d2691b5356182b2c
putiw <pw1246@nyu.edu>
2026-09-25 12:01:04 +0400

    Anchor stimuli explicitly to fixation center

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "rania-radar-";
-const CACHE_NAME = `${CACHE_PREFIX}v22`;
+const CACHE_NAME = `${CACHE_PREFIX}v23`;
 const FACE_ASSET_VERSION = "photos-6";
 const APP_ROOT = new URL("./", self.registration.scope);
 const appUrl = (path = "") => new URL(path, APP_ROOT).href;
